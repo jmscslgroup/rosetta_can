11cc594accdbe8a4bb4c9d6471d3a5c807e803e5
Matthew Nice <matthewwalternice@gmail.com>
2022-07-26 14:45:45 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='rosetta_can',
-      version='0.1',
+      version='0.1.1',
       description='Python CAN Analysis to find Signals',
       author='Matthew Nice',
       author_email='matthew.nice@vanderbilt.edu',
