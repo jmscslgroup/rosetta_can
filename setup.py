8607c3844fea0445f7522f4b966108f3413ae60c
Matthew Nice <matthewwalternice@gmail.com>
2022-07-26 13:47:49 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,12 @@ setup(name='rosetta_can',
       author='Matthew Nice',
       author_email='matthew.nice@vanderbilt.edu',
       url='https://github.com/jmscslgroup/rosetta_can',
-      packages=['rosetta_can'],
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "Framework :: AsyncIO",
+          "Topic :: Communications",
+          "Topic :: Scientific/Engineering :: Visualization",
+          "License :: OSI Approved :: MIT License",
+          ],
+      packages=setuptools.find_packages(),
      )
