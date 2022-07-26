8f5313eb58eac83c3a21130cde5ddc484a6605ab
Matthew Nice <matthewwalternice@gmail.com>
2022-07-26 13:43:36 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='rosetta_can',
+      version='0.1',
+      description='Python CAN Analysis to find Signals',
+      author='Matthew Nice',
+      author_email='matthew.nice@vanderbilt.edu',
+      url='https://github.com/jmscslgroup/rosetta_can',
+      packages=['rosetta_can'],
+     )
